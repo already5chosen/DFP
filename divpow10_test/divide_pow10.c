6203736720006cd08fd91856d62e32b9a714bb2b
Michael S <already5chosen@yahoo.com>
2019-09-02 21:42:58 +0300

    Process n <= 19 with single-pass division.

--- divpow10_test/divide_pow10.c
+++ divpow10_test/divide_pow10.c
@@ -1,5 +1,6 @@
 #include "divide_pow10.h"
 #include <string.h>
+#include <stdio.h>
 
 #ifndef _MSC_VER
 static inline uint64_t __umulh(uint64_t a, uint64_t b) {
@@ -24,28 +25,82 @@ static inline uint64_t __umulh(uint64_t a, uint64_t b) {
 //    in a sense that it causes no memory corruptions, traps or any other undefined actions
 int DivideUint224ByPowerOf10(uint64_t result[2], const uint64_t src[4], unsigned n)
 {
-  if (n <= 1) {
+  if (n <= 19) {
+    // 10**n < 2**64. Single-pass division
+    uint64_t src2 = src[2];
     uint64_t src1 = src[1];
     uint64_t src0 = src[0];
-    uint64_t rem = 0;
-    if (n != 0) {
-      uint64_t d = src1;
-      src1 = d / 10;
-      rem = d - src1*10;
-      d = (rem << 32) | (src0 >> 32);
-      uint64_t src0h = d / 10;
-      rem = d - src0h*10;
-      d = (rem << 32) | (src0 & (uint32_t)(-1));
-      uint64_t src0l = d / 10;
-      rem = d - src0l*10;
-      src0 = (src0h << 32) | src0l;
+    if (n==0) {
+      result[1] = src1;
+      result[0] = src0;
+      return 0;
     }
-    result[0] = src0;
-    result[1] = src1;
-    return rem < 5 ? rem != 0 : (rem != 5) + 2;
+    static const struct {
+      uint64_t invF;
+      uint64_t halfMulF;
+      uint8_t  rshift;
+      uint8_t  rshift2;
+    } recip_tab0[19] = {
+     {0xCCCCCCCCCCCCCCCC,                   10ULL / 2,  3,  0   }, //  1
+     {0xA3D70A3D70A3D70A,                  100ULL / 2,  6,  0   }, //  2
+     {0x83126E978D4FDF3B,                 1000ULL / 2,  9,  9-5 }, //  3
+     {0xD1B71758E219652B,                10000ULL / 2, 13, 13-5 }, //  4
+     {0xA7C5AC471B478423,               100000ULL / 2, 16, 16-5 }, //  5
+     {0x8637BD05AF6C69B5,              1000000ULL / 2, 19, 19-5 }, //  6
+     {0xD6BF94D5E57A42BC,             10000000ULL / 2, 23, 23-5 }, //  7
+     {0xABCC77118461CEFC,            100000000ULL / 2, 26, 26-5 }, //  8
+     {0x89705F4136B4A597,           1000000000ULL / 2, 29, 29-5 }, //  9
+     {0xDBE6FECEBDEDD5BE,          10000000000ULL / 2, 33, 33-5 }, // 10
+     {0xAFEBFF0BCB24AAFE,         100000000000ULL / 2, 36, 36-5 }, // 11
+     {0x8CBCCC096F5088CB,        1000000000000ULL / 2, 39, 39-5 }, // 12
+     {0xE12E13424BB40E13,       10000000000000ULL / 2, 43, 43-5 }, // 13
+     {0xB424DC35095CD80F,      100000000000000ULL / 2, 46, 46-5 }, // 14
+     {0x901D7CF73AB0ACD9,     1000000000000000ULL / 2, 49, 49-5 }, // 15
+     {0xE69594BEC44DE15B,    10000000000000000ULL / 2, 53, 53-5 }, // 16
+     {0xB877AA3236A4B449,   100000000000000000ULL / 2, 56, 56-5 }, // 17
+     {0x9392EE8E921D5D07,  1000000000000000000ULL / 2, 59, 59-5 }, // 18
+     {0xEC1E4A7DB69561A5, 10000000000000000000ULL / 2, 63, 63-7 }, // 19
+    };
+    uint64_t invF = recip_tab0[n-1].invF;
+    uint64_t halfMulF = recip_tab0[n-1].halfMulF;
+    uint64_t mulF = halfMulF+halfMulF;
+    unsigned rshift = recip_tab0[n-1].rshift;
+    unsigned rshift2 = recip_tab0[n-1].rshift2;
+    const uint64_t MSK56 = (uint64_t)-1 >> 8;
+
+    uint64_t d1 = (src1 << 8) | (src0 >> 56);
+    uint64_t d0 = src0 & MSK56;
+    uint64_t r1, r0;
+    if (n <= 4) {
+      if (n <= 2) {
+        r1 = __umulh(d1, invF) >> rshift;
+      } else {
+        r1 = __umulh(src1, invF) >> (rshift-8);
+      }
+    } else {
+      // src2 contains at most rshift+1-16 significant bits
+      // It can be shifted to the left by 64-(rshift+1-16)=79-rshift bits
+      uint64_t dh1 = (src2 << (79-rshift)) | (src1 >> (rshift-15));
+      r1 = __umulh(dh1, invF) >> 7;
+    }
+    uint64_t rem0 = d1 - r1*mulF;
+    uint64_t d0h = (rem0 << (56-rshift2)) | (d0 >> rshift2);
+    d0 |= (rem0 << 56);
+    r0 = __umulh(d0h, invF) >> (rshift-rshift2);
+    uint64_t rem = d0 - r0*mulF;
+    // if (n==19)printf("rem0=%20llu rem=%20llu\n", rem0, rem);
+    // while (rem >= mulF) {
+    if (rem >= mulF) {
+      rem -= mulF;
+      r0  += 1;
+    }
+    r1 += (r0 >> 56); r0 &= MSK56;
+    result[0] = (r1 << 56) | r0;
+    result[1] = r1 >> 8;
+    return rem < halfMulF ? (rem != 0) : (rem != halfMulF) + 2;
   }
 
-  // n > 1
+  // n > 19
   // Divide source by 2**(n-1)
   // Result of division contains at most 191 bit, so it fits easily in 3 64-bit word
   uint64_t src2 = (src[3] << (65-n)) | (src[2] >> (n-1));
